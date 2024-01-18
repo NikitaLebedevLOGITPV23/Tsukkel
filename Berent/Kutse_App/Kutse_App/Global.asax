2961ba0b2d31b103325ff9e51c696bcbfc414249
opilane <opilane@HP-CZC2349HT2>
2024-01-18 12:41:02 +0200

    T

--- Berent/Kutse_App/Kutse_App/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kutse_App.MvcApplication" Language="C#" %>
---
+++ DavidTARPV23/PythonApplication david Lennuk/PythonApplication david Lennuk/PythonApplication_david_Lennuk.py
@@ -0,0 +1,24 @@
+﻿print("Tere! Olen sinu uus sõber -Python!")
+nimi=input("Mis on sinu nimi?")
+print(nimi+", oi kui ilus nimi!")
+number=(int(input(nimi+"! Kas leian Sinu keha indeksi?i 0-ei, 1-jah =>")))
+if number==1:
+    pikkus=float(input("Pikkus= "))
+    mass=float(input("mass= "))
+    indeks=mass/(0.01*pikkus)**2
+    print("!Sinu keha indeks on:", indeks)
+if indeks<16:
+    print("Tervisele ohtlik alakaal")
+elif indeks>16 and indeks<19:
+    print("Alakaal")
+elif indeks>19 and indeks<25:
+    print("Normaalkaal")
+elif indeks>25 and indeks<30:
+    print("Ülekaal")
+elif indeks>30 and indeks<35:
+    print("Rasvumine")
+elif indeks>35 and indeks<40:
+    print("Tugev rasvumine")
+elif indeks>40:
+    print("Tervisele ohtlik rasvumine")
+
